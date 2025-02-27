4a097ef74adec5473548c6e4bf6dc4fc91f88c93
Elai5 <yvettelaine007@gmail.com>
2025-02-27 15:08:04 +0300

    drinking game. figured to incoporate components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React from "react";
 
+function coinToss() {
+  return Math.random() < 0.5 ? "heads" : "tails";
+}
+
+const pics = {
+  kitty: "https://content.codecademy.com/courses/React/react_photo-kitty.jpg",
+  doggy: "https://content.codecademy.com/courses/React/react_photo-puppy.jpeg",
+};
+let age = 18;
+let drinkingAge = 20;
 function App() {
   const myList = (
     <ul>
       <li>finish react</li>
       <li>do cybersec</li>
+      <li>the shawn mendez</li>
     </ul>
   );
 
-  return myList;
+  let img;
+  if (coinToss() === "heads") {
+    img = <img src={pics.kitty} alt="Kitty" />;
+  } else {
+    img = <img src={pics.doggy} alt="Doggy" />;
+  }
+
+  const headline = (
+    <h1>
+      {age >= drinkingAge ? 'Buy drink' : 'do teen stuff'}
+    </h1>
+  );
+  return (
+    <div>
+      {myList}
+      {img}
+      {headline}
+    </div>
+  );
 }
-
-
 
 export default App;
