421c7f7a6cac1d6e5db0a563a1ca0cdcd794f953
Elai5 <yvettelaine007@gmail.com>
2025-02-27 12:55:32 +0300

    Merge inner repository into main

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  const myList = (
+    <ul>
+      <li>finish react</li>
+      <li>do cybersec</li>
+    </ul>
+  );
+
+  return myList;
+}
+
+export default App;
